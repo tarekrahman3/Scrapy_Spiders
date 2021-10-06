c5db27d470940e3b5ecb1f11c4ca769821823ff2
Tarek R <44648395+tarekrahman3@users.noreply.github.com>
2021-10-07 01:14:13 +0600

    Create WashingtonDC_Spider.py

---
+++ after55.com/WashingtonDC_Spider.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import scrapy
+from scrapy import Request
+
+class WashingtonDcSpider(scrapy.Spider):
+    name = 'Washington_DC'
+    allowed_domains = ['after55.com']
+    start_urls = ['https://www.after55.com/dc/washington']
+
+    def parse(self, response):
+        urls = response.xpath("//span[@class='org']/a/@href").getall()
+        for url in urls:
+            print(url)
+            request = Request(url, callback = self.parse_appointments)
+            yield request
+        
+        next_page = response.xpath('//div[contains(@class,"serpPagination")]//a[text()=" NEXT "]/@href').get()
+        if next_page != '':
+            request_ = Request(next_page, callback = self.parse)
+            yield request_
+        else:
+            pass
+    
+    def parse_appointments(self, response):
+        name = response.xpath('//div[@class = "profile-h1"]/h1/text()').get()
+        street_address = response.xpath('//div[@class="adr fn prop_addr"]/span[@class="street-address"]/text()').get()
+        city = response.xpath('//div[@class="adr fn prop_addr"]/span[@class="locality"]/text()').get()
+        state = response.xpath('//div[@class="adr fn prop_addr"]/span[@class="region"]/text()').get()
+        zip_code = response.xpath('//div[@class="adr fn prop_addr"]/span[@class="postal-code"]/text()').get()
+        phone = response.xpath('//span[@class="phone_block"]/a[@id="phonebutton"]/text()').get()
+        yield {
+        'source':response.url,
+        'name':name,
+        'street_address':street_address,
+        'city':city,
+        'state':state,
+        'zip_code':zip_code,
+        'phone':phone
+        }
+
